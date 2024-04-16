172b324f05eec0a5b0090f22f90606e1cdb3809c
Dexter Crago <jodieanddexter@gmail.com>
2024-04-16 09:38:10 -0600

    Add routing for all pages

--- getFile.js
+++ getFile.js
@@ -6,9 +6,16 @@ export default async function getFile(path) {
   try {
     if (path === '/') {
       requestedFile = await fs.readFile('./index.html');
+    } else if (path === '/about') {
+      requestedFile = await fs.readFile('./about.html');
+    } else if (path === '/contact-me') {
+      requestedFile = await fs.readFile('./contact-me.html');
+    } else {
+      requestedFile = await fs.readFile('./404.html');
     }
   } catch (err) {
     console.error(err);
+    requestedFile = await fs.readFile('./404.html');
   }
 
   return requestedFile;
